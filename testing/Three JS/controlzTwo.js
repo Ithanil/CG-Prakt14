ccb86fafbca3e4900d57db58445e5fdbb56b499c
Tombo123 <tombo@rocketmail.com>
2014-04-10 12:27:21 +0200

    more fixes

--- testing/Three JS/controlzTwo.js
+++ testing/Three JS/controlzTwo.js
@@ -104,11 +104,11 @@ function keyDown(event) {
 			physobjs[0].velocity.x=V0*Math.sin(Math.PI/180.0*angle);
 			physobjs[0].velocity.y=0;
 			physobjs[0].velocity.z=-V0*Math.cos(Math.PI/180.0*angle);
-			if(angularVelocity[0]!=0)physobjs[0].anglvel.x=-angularVelocity[0]*30;
+			if(angularVelocity[0]!=0)physobjs[0].anglvel.x=-angularVelocity[0]*angVelocity;
 			else physobjs[0].anglvel.x=0;
-			if(angularVelocity[1]!=0)physobjs[0].anglvel.y=-angularVelocity[1]*30;
+			if(angularVelocity[1]!=0)physobjs[0].anglvel.y=-angularVelocity[1]*angVelocity;
 			else physobjs[0].anglvel.y=0;
-			if(angularVelocity[2]!=0)physobjs[0].anglvel.z=-angularVelocity[2]*30;
+			if(angularVelocity[2]!=0)physobjs[0].anglvel.z=-angularVelocity[2]*angVelocity;
 			else physobjs[0].anglvel.z=0;
 			physobjs[0].updateObject3D();
 			thrown=true;
